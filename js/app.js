d8495729b3f25cae42dabbe5622f96bdb9e204ae
Unknown <llogan382@gmail.com>
2018-09-16 07:03:20 -0400

    Fades in the ID over 2 seconds w Jquery

--- js/app.js
+++ js/app.js
@@ -1,21 +1,2 @@
-//remove the green box with Javascript
-var box = document.querySelector('.box');
-// box.style.display = 'none';
-
-//hide the green box w Jquery
-//jQuery('.box').hide();
-
-//Use the $ instead of the word JQUERY
-// $('.box').hide();
-
-// $('.box').show();
-
-
-// box.addEventListener('click', function() {
-//   alert('You clicked the button!');
-// });
-
-// Same as above w Jquery
-$('.box').click(function() {
-  alert('You clicked the button w Jquery!');
-} );
+$('#flashMessage').hide();
+$('#flashMessage').fadeIn(2000);
