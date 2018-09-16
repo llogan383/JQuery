7e41d813de53d13df787c7d9b5d8204ab55450bf
Unknown <llogan382@gmail.com>
2018-09-16 07:50:06 -0400

    Displays the input from form in the preview section

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-//When running HIDE, JQUERY return the reference to FLASHMESSAGE;
-//then HIDE references that same method
+// //When running HIDE, JQUERY return the reference to FLASHMESSAGE;
+// //then HIDE references that same method
 
-//This is called "Chaining"
-$('#flashMessage')
-  .hide()
-  .slideDown(2000)
-  .delay(3000)
-  .slideUp(2000);
+// //This is called "Chaining"
+// $('#flashMessage')
+//   .hide()
+//   .slideDown(2000)
+//   .delay(3000)
+//   .slideUp(2000);
 
-const title = "my first blog post";
-const content = "this is my <strong>first</strong> post";
+// const title = "my first blog post";
+// const content = "this is my <strong>first</strong> post";
 
-//puts the title in the ID listed
-$('#blogTitlePreview')
-  .text(title);
-
-  //puts the content in the ID listed. 
-$('#blogContentPreview')
-  .html(content);
+//Add an event listener for the button 
+$('#previewButton').click(function(){
+  //store the ID value in 'title; using the VAL method,
+  //which returns what is entered in the form
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+  //takes the input from above and puts it into the 
+  //id listed
+  $('#blogTitlePreview')
+    .text(title);
+  $('#blogContentPreview')
+    .html(content);
+});
