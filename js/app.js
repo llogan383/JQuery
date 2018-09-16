50d6e9fdbcb12f80dff0f54e5d6b63f31440e224
Unknown <llogan382@gmail.com>
2018-09-16 08:39:07 -0400

    Hides spoiler on pageload; reveals on click

--- js/app.js
+++ js/app.js
@@ -1,32 +1,11 @@
-// //When running HIDE, JQUERY return the reference to FLASHMESSAGE;
-// //then HIDE references that same method
+//hide the spoiler on page load
+$('.spoiler span')
+  .hide();
 
-// //This is called "Chaining"
-
-
-// const title = "my first blog post";
-// const content = "this is my <strong>first</strong> post";
-
-//Hide the message on page load;
-$('#flashMessage')
-.hide();
-
-//Add an event listener for the button 
-$('#previewButton').click(function(){
-  //store the ID value in 'title; using the VAL method,
-  //which returns what is entered in the form
-  const title = $('#blogTitleInput').val();
-  const content = $('#blogContentInput').val();
-  //takes the input from above and puts it into the 
-  //id listed
-  $('#blogTitlePreview')
-    .text(title);
-  $('#blogContentPreview')
-    .html(content);
-
-    //display message when content is saved. 
-  $('#flashMessage')
-    .slideDown(2000)
-    .delay(3000)
-    .slideUp(2000);
-});
+//reveal spoiler and hide button on click
+$('.spoiler button').click(function(){
+  $('.spoiler span')
+    .fadeIn(2000);
+  $('.spoiler button')
+    .hide();
+})
