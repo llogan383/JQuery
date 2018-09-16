5e6ffe21e93afc01e4376faa810d70b02d41d413
Unknown <llogan382@gmail.com>
2018-09-16 07:52:00 -0400

    Adds message that displays when content is saved

--- js/app.js
+++ js/app.js
@@ -2,11 +2,7 @@
 // //then HIDE references that same method
 
 // //This is called "Chaining"
-// $('#flashMessage')
-//   .hide()
-//   .slideDown(2000)
-//   .delay(3000)
-//   .slideUp(2000);
+
 
 // const title = "my first blog post";
 // const content = "this is my <strong>first</strong> post";
@@ -23,4 +19,10 @@ $('#previewButton').click(function(){
     .text(title);
   $('#blogContentPreview')
     .html(content);
+
+    $('#flashMessage')
+  .hide()
+  .slideDown(2000)
+  .delay(3000)
+  .slideUp(2000);
 });
