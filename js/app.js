b24525fa6082afe900196a91b643bffc714ef543
Unknown <llogan382@gmail.com>
2018-09-17 05:19:09 -0400

    Appends html using Jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//show button if JS is available
+//create html
+
+//using the "$" before variable shows that it is a jQuery variable
+const $button = $('<button> Reveal Spoiler</button>');
+$('.spoiler').append($button);
+
 //hide the spoiler on page load
 $('.spoiler span')
   .hide();
