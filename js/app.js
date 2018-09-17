7cf83a24d2baae8069aa8597309ab6161263b73f
Unknown <llogan382@gmail.com>
2018-09-17 05:42:55 -0400

    Moves the code to header; add event delegation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+
+//reveal spoiler and hide button on click
+$('.spoiler button').click(function(){
+  $('.spoiler span')
+    .fadeIn(2000);
+  $('.spoiler button')
+    .hide();
+})
+
+//below: the code basically says, identify this class (spoiler)
+//then, if there is an event (click) on the element (button),
+//then do this code. 
+$('.spoiler').on('click', 'button', function() )
+
 //show button if JS is available
 //create html
 
@@ -9,10 +23,4 @@ $('.spoiler').append($button);
 $('.spoiler span')
   .hide();
 
-//reveal spoiler and hide button on click
-$('.spoiler button').click(function(){
-  $('.spoiler span')
-    .fadeIn(2000);
-  $('.spoiler button')
-    .hide();
-})
+
