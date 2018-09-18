0ef5dda5d1363c1238cc45d10a1265df742ea803
Unknown <llogan382@gmail.com>
2018-09-18 06:01:16 -0400

    Creates an attribute for links in new tab

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ const $odd = $('a:odd');
 
 //select all links that start wtih https
 const $secureLinks = $('a[href^="https://"]');
+
+//selects all links ending with .pdf
+const $pdfs = $('a[href$=".pdf"]');
+
+$secureLinks.attr('target', '_blank');
