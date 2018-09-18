af162c77b25b43fb04031df76b160da41f068fb9
Unknown <llogan382@gmail.com>
2018-09-18 05:41:42 -0400

    Select all secure links

--- js/app.js
+++ js/app.js
@@ -1,23 +1,4 @@
-//below: the code basically says, identify this class (spoiler)
-//then, if there is an event (click) on the element (button),
-//then do this code. 
-$('.spoiler').on('click', 'button', function(event){
-  console.log(event.target);
-  //tells jquery, whatever button was clicked,
-  //go to the previous element using the prev() function
-  $(this).prev().show();
-    $(this).hide();
-} );
+const $odd = $('a:odd');
 
-//show button if JS is available
-//create html
-
-//using the "$" before variable shows that it is a jQuery variable
-const $button = $('<button> Reveal Spoiler</button>');
-$('.spoiler').append($button);
-
-//hide the spoiler on page load
-$('.spoiler span')
-  .hide();
-
-
+//select all links that start wtih https
+const $secureLinks = $('a[href^="https://"]');
