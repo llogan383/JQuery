4d2fd3e962e5276cf80c519f61e30dcac1f42af9
Unknown <llogan382@gmail.com>
2018-09-20 05:53:23 -0400

    Only allow downloads if box is checked

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const $odd = $('a:odd');
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
+
 
 //select all links that start wtih https
 const $secureLinks = $('a[href^="https://"]');
@@ -10,3 +12,19 @@ $secureLinks.attr('target', '_blank');
 
 //cuase .pdf files to be donwloaded when clicked; DOESNT WORK IN CHROME
 $pdfs.attr( 'download', true );
+
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event){
+
+    //checks if checkbox is chcked
+    if ($(':checked').length === 0){
+        //prevent the default browser behavior. There are dozens of
+        //default methods to choose from. 
+        event.preventDefault();
+        //alert the user
+        alert('Please check the box to allow PDF downloads');
+    } 
+});
+$('#links').append($pdfCheckbox);
